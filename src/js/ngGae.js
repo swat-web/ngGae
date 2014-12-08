c8b6d222cf63c189856fdc300c6228888c71be34
Vlad Tansky <vl.tansky@gmail.com>
2014-12-08 18:08:24 +0200

    v1

---
+++ src/js/ngGae.js
@@ -0,0 +1,83 @@
+(function(window, angular, undefined) {
+    'use strict';
+
+    angular.module('ngGae', [])
+        .factory('GapiService', ['$q', function($q) {
+            var config = {
+                    'client_id': '',//client id from google console
+                    'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                    'immediate': 'false'
+                },
+                LoginStatus = false;
+
+            return {
+                /**
+                 * Config: set google app client ID
+                 * @param {string} value - Client ID from your google app
+                 */
+                setClientId: function(value) {
+                    config.client_id = value;
+                },
+                /**
+                 * Config: set scopes for your app
+                 * @param {string} value - permissions (scopes) that you request from your user
+                 */
+                setScopes: function(value) {
+                    config.scope = value;
+                },
+                /**
+                 * Config: set immediate mode
+                 * @param {boolean} value
+                 */
+                setImmediate: function(value) {
+                    config.immediate = value;
+                },
+                /**
+                 * Get login status
+                 * @return {boolean} return login status
+                 */
+                getLoginStatus: function() {
+                    return LoginStatus;
+                },
+                /**
+                 * Signing in user to google account and execute callback functions
+                 * @return {object} return promise
+                 */
+                auth: function() {
+                    var deferred = $q.defer();
+                    gapi.auth.authorize(config, function() {
+                        LoginStatus = true;
+                        deferred.resolve(true);
+                    });
+                    return deferred.promise;
+                },
+
+                /**
+                 * Initializes the application. It loads asynchronously all needed libraries
+                 * @param {array} endPoints - parameters for gapi.client.load()
+                 *
+                 * @return {object} return promise
+                 */
+                init: function(endPoints) {
+                    var deferred = $q.defer();
+                    var promises = [];
+
+                    angular.forEach( endPoint, function(src, version, root){
+                        promises.push(gapi.client.load(src, version, null, root));
+                    });
+
+                    $q.all(promises).then(function(){
+                        deferred.resolve();
+                    });
+                    return deferred.promise;
+
+                }
+            };
+
+        }])
+
+})(window, window.angular);
+
+function init() {
+    window.ngGae_init();
+}
