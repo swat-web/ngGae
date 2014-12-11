7dfe7d147b0c4e6f0c7b23c1b8730caec473f15a
Vlad Tansky <vl.tansky@gmail.com>
2014-12-11 05:43:50 +0200

    grunt updated

--- gruntfile.js
+++ gruntfile.js
@@ -18,38 +18,13 @@ module.exports = function(grunt) {
                     ext: '.min.js'
                 }]
             }
-        },
-        cssmin: {
-            my_target: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/css/',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'dist/css/',
-                    ext: '.min.css'
-                }]
-            }
-        }//,
-        //concat: {
-        //    options: {
-        //        separator: ';'
-        //    },
-        //
-        //    basic_and_extras: {
-        //        files: {
-        //            'dist/js/application.min.js': [],
-        //            'dist/css/style.min.css': []
-        //        }
-        //    }
-        //}
+        }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'cssmin', 'concat']);
+    grunt.registerTask('default', ['uglify']);
 
 };
